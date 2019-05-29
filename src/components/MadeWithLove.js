82dc2332068ddbe7670cbec775590063b1470fc8
Frederik Bolding <frederik.bolding@gmail.com>
2019-05-29 14:09:04 +0200

    Fixed formatting

--- src/components/MadeWithLove.js
+++ src/components/MadeWithLove.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class MadeWithLove extends Component {
   render() {
     return (
       <div style={this.props.style} className="made-with-love">
-        Made with <span role='img' aria-label='heart'>❤️</span> by <a href={this.props.link}>{this.props.by}</a>
+        Made with{" "}
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>{" "}
+        by <a href={this.props.link}>{this.props.by}</a>
       </div>
-    );
+    )
   }
 }
 
-export default MadeWithLove;
+export default MadeWithLove
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import { Component } from "react"
 
 import SEO from "../components/seo"
 import MadeWithLove from "../components/MadeWithLove"
-import TimeAgo from 'react-timeago'
-import GithubCorner from 'react-github-corner';
+import TimeAgo from "react-timeago"
+import GithubCorner from "react-github-corner"
 
 import "../components/index.css"
 
@@ -15,11 +15,11 @@ const CoinGeckoClient = new CoinGecko()
 class IndexPage extends Component {
   componentDidMount() {
     this.fetchPrice()
-    this.interval = setInterval(() => this.fetchPrice(), 60 * 1000);
+    this.interval = setInterval(() => this.fetchPrice(), 60 * 1000)
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   state = {
@@ -34,21 +34,31 @@ class IndexPage extends Component {
         <div className="App">
           <div className="App-content">
             {!this.state.data ? (
-              <h1 className="loading">{!this.state.error ? ("Loading") : ("Error")}</h1>
+              <h1 className="loading">
+                {!this.state.error ? "Loading" : "Error"}
+              </h1>
             ) : (
               <>
                 <h2 className="header">{this.state.crypto}</h2>
                 <h1 className="price">${this.state.data.usd.toFixed(2)}</h1>
-                <h2 style={{ color : (this.state.data.usd_24h_change > 0 ? "#44be24" : "#ef4f1b")}}>
+                <h2
+                  style={{
+                    color:
+                      this.state.data.usd_24h_change > 0
+                        ? "#44be24"
+                        : "#ef4f1b",
+                  }}
+                >
                   {this.state.data.usd_24h_change > 0 ? "+" : ""}
                   {this.state.data.usd_24h_change.toFixed(3)}%
                 </h2>
                 <div className="last-updated">
-                Last updated: <TimeAgo date={this.state.data.last_updated_at*1000} />
+                  Last updated:{" "}
+                  <TimeAgo date={this.state.data.last_updated_at * 1000} />
                 </div>
               </>
             )}
-            
+
             <MadeWithLove
               by="Frederik Bolding"
               link="https://frederikbolding.com"
@@ -61,15 +71,21 @@ class IndexPage extends Component {
   }
 
   fetchPrice = async () => {
-    let crypto = this.props["*"] ? this.props["*"] : "ethereum";
+    let crypto = this.props["*"] ? this.props["*"] : "ethereum"
     let data = await CoinGeckoClient.simple.price({
       ids: [crypto],
       vs_currencies: ["eur", "usd"],
       include_24hr_change: true,
       include_last_updated_at: true,
     })
-    let error = !data.data[crypto];
-    this.setState({ crypto: error ? this.state.crypto : crypto.charAt(0).toUpperCase() + crypto.slice(1), data: data.data[crypto], error: error })
+    let error = !data.data[crypto]
+    this.setState({
+      crypto: error
+        ? this.state.crypto
+        : crypto.charAt(0).toUpperCase() + crypto.slice(1),
+      data: data.data[crypto],
+      error: error,
+    })
   }
 }
 
